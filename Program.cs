6f5faad3c1ac5d6a79b56454e686c3f9af555b55
Andronache <cristian_and19@hotmail.com>
2023-06-17 15:29:56 -0400

    first commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+string version = "v1";
+string title = "Atelier";
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c=> c.SwaggerDoc(version, 
+    new() {Title = title, Version = version }));
+
+var app = builder.Build();
 
 app.Run();
